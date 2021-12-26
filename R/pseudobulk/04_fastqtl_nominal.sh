29f0426e8c73923cce1800170d0463f4a9c1d425
apnathan <aparnanathan@g.harvard.edu>
2021-12-26 12:28:39 -0500

    Create 04_fastqtl_nominal.sh

---
+++ R/pseudobulk/04_fastqtl_nominal.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Script to run FastQTL (Ongen 2015, Bioinformatics)
+
+# VCF: post QC and imputation genotype dosage VCF (genotype data from dbGaP: phs002025)
+# BED: output from previous script
+# Region: select chromosome
+# out: output file
+
+chr=$1
+
+mkdir -p /path/fastqtl_nominal/chr$chr
+
+/path/fastQTL \
+   --vcf  /path/geno_imputed_dosage_maf05_hg38_QC.vcf.gz \
+   --bed  /path/chr${chr}_input.bed.gz   \
+   --region  chr$chr \
+   --out  /path/fastqtl_nominal/chr$chr/chr${chr}_output.nominal.tbru.txt
+
+gzip -f /path/fastqtl_nominal/chr$chr/chr${chr}_output.nominal.tbru.txt
